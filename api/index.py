a81421fd6279221a73d1018b10b11fcc33aa2e3e
Sejal Khatri <sejalkhatri@dhcp-10-250-61-27.harvard.edu>
2025-09-15 16:54:06 -0400

    Fix Base64 bug: invalid inputs like 'python3 -m pytest -v!' now raise an error instead of decoding to zero

--- api/index.py
+++ api/index.py
@@ -37,8 +37,8 @@ def base64_to_number(b64_str):
     """Convert base64 to integer"""
     try:
         # Decode base64 to bytes, then convert bytes to integer
-        decoded_bytes = base64.b64decode(b64_str)
-        return int.from_bytes(decoded_bytes, byteorder='big')
+        decoded_bytes = base64.b64decode(b64_str, validate=True)
+        return int.from_bytes(decoded_bytes, byteorder='little')
     except:
         raise ValueError("Invalid base64 input")
 
@@ -47,7 +47,7 @@ def number_to_base64(number):
     try:
         # Convert integer to bytes, then encode to base64
         byte_count = (number.bit_length() + 7) // 8
-        number_bytes = number.to_bytes(byte_count, byteorder='big')
+        number_bytes = number.to_bytes(byte_count, byteorder='little')
         return base64.b64encode(number_bytes).decode('utf-8')
     except:
         raise ValueError("Unable to convert to base64")
